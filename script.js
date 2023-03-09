e108f06a23c7a36fcca907c4b8c19e55e35d49d7
LOrellanae <lorellanae2@miumg.edu.gt>
2023-03-09 16:00:44 -0600

    Se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,34 @@
+const tasksContainer = document.getElementById('tasksContainer');
+
+const addNewTask = event => {
+    event.preventDefault();
+    const { value } = event.target.taskText;
+    if(!value) return;
+    const taskopcontainer = document.createElement('div');
+    taskopcontainer.classList.add('taskopcontainer');
+    tasksContainer.prepend(taskopcontainer);
+    
+    const task = document.createElement('div');
+    task.classList.add('task', 'roundBorder');
+    task.addEventListener('click', changeTaskState)
+    task.textContent = value;
+    taskopcontainer.prepend(task);
+
+    const remtask = document.createElement('div');
+    remtask.classList.add('remtask', 'roundBorder');
+    remtask.addEventListener('click', removeTask)
+    remtask.textContent = 'X';
+    taskopcontainer.append(remtask);
+
+    event.target.reset();
+};
+
+const removeTask = event => {
+    event.target.parentElement.remove()
+}
+
+const changeTaskState = event => {
+    event.target.classList.toggle('done');
+};
+
+setDate();
